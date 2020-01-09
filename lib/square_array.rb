258af1937a2a1c89cb92f58ea4e5ca63ec47929a
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-09 17:56:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   counter = 0
 
   while array.length > counter do
-    array[counter] = array[counter]**array[counter]
+    array[counter] = array[counter]*array[counter]
     counter+=1
   end
-  p array
+  return array
 end
